e4fc07cff60a39e71a51e99583296885943e3c80
1noseA <1noseA2L@gmail.com>
2020-07-29 20:28:57 +0900

    [Add]モデルにリレーション追加

--- app/Models/Follower.php
+++ app/Models/Follower.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Follower extends Model
 {
-    //
+    protected $primaryKey = [
+        'following_id',
+        'followed_id'
+    ];
+    protected $fillable = [
+        'following_id',
+        'followed_id'
+    ];
 }
