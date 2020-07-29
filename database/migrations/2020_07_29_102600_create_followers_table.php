61497120c8b03a48e9d9c5bf295a034af1b91492
1noseA <1noseA2L@gmail.com>
2020-07-29 20:18:53 +0900

    [Add]全テーブル追加

---
+++ database/migrations/2020_07_29_102600_create_followers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('following_id')->comment('フォローしているユーザID');
+            $table->foreignId('followed_id')->comment('フォローされているユーザID');
+            $table->timestamps();
+
+            $table->unique([
+                'following_id',
+                'followed_id'
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
