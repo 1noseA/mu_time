ece1e5c470add6ce8a739bf1a24f4a63763308f9
1noseA <1noseA2L@gmail.com>
2020-08-01 13:09:53 +0900

    [Add]tweet/show

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -10,11 +10,6 @@ use App\Models\Follower;
 
 class TweetsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(Tweet $tweet, Follower $follower)
     {
         $user = auth()->user();
@@ -30,67 +25,39 @@ class TweetsController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function show(Tweet $tweet, Comment $comment)
+    {
+        $user = auth()->user();
+        $tweet = $tweet->getTweet($tweet->id);
+        $comments = $comment->getComments($tweet->id);
+
+        return view('tweets.show', [
+            'user'     => $user,
+            'tweet' => $tweet,
+            'comments' => $comments
+        ]);
+    }
+    
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
