50c7c44b66a1fc4f3d910e358ba4489024441b7a
1noseA <1noseA2L@gmail.com>
2020-08-03 15:15:48 +0900

    [Fix]日本語化

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,7 +4,7 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-10 my-5">
-        <h3 class="text-center mb-3">ユーザー一覧</h3>
+        <h3 class="text-center mb-3">ユーザ一覧</h3>
         @foreach ($all_users as $user)
           <div class="card">
             <div class="card-haeder p-3 w-100 d-flex">
